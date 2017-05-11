b203b4ab6f8f8c179b0b851fce91b62f18e7ec65
Nikolay Yushkevich <n.yushkevich@hotmail.com>
2017-05-11 20:00:17 +0300

    Initial commit

---
+++ RailRoadCounter/RailRoadCounterPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace RailRoadCounter
+{
+	public partial class RailRoadCounterPage : ContentPage
+	{
+		public RailRoadCounterPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
