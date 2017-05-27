cf5e4044f5aef4f287a204efa24075c885584df3
BulatMukhutdinov <Bulatmm@yandex.ru>
2017-05-27 13:41:05 +0300

    Splashscreen

--- Droid/SplashActivity .cs
+++ Droid/SplashActivity .cs
@@ -1,11 +1,13 @@
 ﻿using Android.App;
+using Android.OS;
 using Android.Support.V7.App;
 
 namespace RailRoadCounter.Droid
 {
     [Activity(Label = "RailRoadCounter.Droid", Icon = "@drawable/icon", Theme = "@style/splashscreen", MainLauncher = true, NoHistory = true)]
-    public class SplashActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    public class SplashActivity : AppCompatActivity
     {
+        
         protected override void OnResume()
         {
             base.OnResume();
---
+++ RailRoadCounter/Views/Input.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RailRoadCounter.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Input : ContentPage
+    {
+        public Input()
+        {
+            InitializeComponent();
+        }
+    }
+}
